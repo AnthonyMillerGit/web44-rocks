08c8e4cab559dd30f52923e5511651a4c7939bd5
ferengitooth <ehitsamill@gmail.com>
2021-08-05 10:19:51 -0700

    wip

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('web 44 rocks')
+require('dotenv').config() // this allows to inject fake environment variables
+
+const { PORT } = require('./config')
+const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const server = express()
+
+server.use(express.json())
+server.use(cors())
+server.use(helmet())
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>Web 44 is awesome</h1>
+    `)
+})
+
+server.use('*', (req, res) => {
+    res.json({ 
+        message: 'web44 rocks!'
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
